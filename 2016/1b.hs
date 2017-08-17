c6fbcd8b69f40f02240abfc05f4c8800bd91bb50
Joel Brady <joel.brady1@gmail.com>
2017-08-17 23:38:22 +1000

    1b haskell

--- 2016/1b.hs
+++ 2016/1b.hs
@@ -2,14 +2,13 @@
 
 import qualified Data.Set as Set
 import qualified Data.Text as Text
-import qualified Data.Text.Read as R
+import qualified Data.Text.Read as Read
 import qualified Debug.Trace as Trace
 
 data Direction = L | R deriving Show
-data Orientation = N | S | E | W deriving Show
 data Instruction = Instruction Direction Int deriving Show
-data Coordinates = Coordinates (Int, Int) deriving (Show, Eq, Ord)
-data State = State Coordinates Orientation (Set.Set Coordinates) (Maybe Coordinates) deriving Show
+data State = State Vector Vector (Set.Set Vector) deriving Show
+data Vector = Vector Int Int deriving (Show, Eq, Ord)
 
 direction :: Char -> Direction
 direction c
@@ -24,46 +23,48 @@ instruction text =
     in Instruction d n
 
 textToInt :: Text.Text -> Int
-textToInt text = case R.decimal text of Right (n, _) -> n
-                                        Left err -> error err
+textToInt text = case Read.decimal text of Right (n, _) -> n
+                                           Left err -> error err
 
-distanceFromOrigin :: Coordinates -> Int
-distanceFromOrigin (Coordinates (x, y)) = abs(x) + abs(y)
+distanceFromOrigin :: Vector -> Int
+distanceFromOrigin (Vector x y) = abs(x) + abs(y)
 
--- TODO use vector instead
-rotate :: Orientation -> Direction -> Orientation
-rotate N L = W
-rotate N R = E
-rotate E L = N
-rotate E R = S
-rotate S L = E
-rotate S R = W
-rotate W L = S
-rotate W R = N
+rotate :: Vector -> Direction -> Vector
+rotate o R = rotateRight o
+rotate o L = rotateLeft o
 
-moveForward :: Coordinates -> Orientation -> Int -> Coordinates
-moveForward (Coordinates (x, y)) N n = Coordinates (x, y + n)
-moveForward (Coordinates (x, y)) S n = Coordinates (x, y - n)
-moveForward (Coordinates (x, y)) E n = Coordinates (x + n, y)
-moveForward (Coordinates (x, y)) W n = Coordinates (x - n, y)
+rotateRight :: Vector -> Vector
+rotateRight (Vector x y) = Vector y (-x)
 
-f :: State -> Instruction -> State
-f (State c o v m) (Instruction d n) =
-    case m of Just _ -> State c o v m
-              Nothing ->
-                    let newOrientation = rotate o d
-                        newCoordinates = moveForward c newOrientation n
-                        newVisitedSet  = Set.insert newCoordinates v
-                        maybeVisitedTwice = if Set.member newCoordinates v then
-                                                Trace.trace "blah" $ Just newCoordinates
-                                            else
-                                                Trace.trace "nothing" Nothing
-                    in State newCoordinates newOrientation newVisitedSet maybeVisitedTwice
+rotateLeft :: Vector -> Vector
+rotateLeft = rotateRight . rotateRight . rotateRight
 
-initialCoordinates = Coordinates (0, 0)
-initialDirection   = N
+add :: Vector -> Vector -> Vector
+add (Vector a1 b1) (Vector a2 b2) = Vector (a1 + a2) (b1 + b2)
+
+moveForward :: Vector -> Vector -> Int -> Set.Set Vector-> (Vector, Set.Set Vector, Maybe Vector)
+moveForward coordinates _ 0 visitedSet = (coordinates, visitedSet, Nothing)
+moveForward start orientation steps visitedSet =
+    let newPosition = start `add` orientation
+    in
+        if Set.member newPosition visitedSet then
+            (newPosition, visitedSet, Just newPosition)
+        else
+            moveForward newPosition orientation (steps - 1) (Set.insert newPosition visitedSet)
+
+f :: State -> [Instruction] -> State
+f s [] = s
+f (State coordinates orientation visited) ((Instruction d n):is) =
+    let newOrientation = rotate orientation d
+        (newCoordinates, newVisitedSet, possibleSolution) = moveForward coordinates newOrientation n visited
+        nextState = State newCoordinates newOrientation newVisitedSet
+    in case possibleSolution of Just _  -> nextState
+                                Nothing -> f nextState is
+
+initialCoordinates = Vector 0 0
+initialDirection   = Vector 0 1
 initialVisitedSet  = Set.empty
-startState         = State initialCoordinates initialDirection initialVisitedSet Nothing
+startState         = State initialCoordinates initialDirection initialVisitedSet
 inputFilename      = "1b.input"
 
 rawInputStringToTokens :: Text.Text -> [Text.Text]
@@ -76,19 +77,12 @@ main :: IO ()
 main = do
     rawInput <- readFile inputFilename
     let moves = parseInput $ Text.pack rawInput
-    let endPosition = finalPosition startState moves
+    let endPosition = f startState moves
     let distance = distanceFromOrigin $ getCurrentCoordinates endPosition
-    let solution = getSolution endPosition
-    putStrLn $ show solution
+    putStrLn $ show distance
 
-finalPosition :: State -> [Instruction] -> State
-finalPosition start moves = foldl f start moves
+getCurrentCoordinates :: State -> Vector
+getCurrentCoordinates (State c _ _) = c
 
-getCurrentCoordinates :: State -> Coordinates
-getCurrentCoordinates (State c _ _ _) = c
-
-getSolution :: State -> Maybe Coordinates
-getSolution (State _ _ _ m) = m
-
-getVistedSet :: State -> Set.Set Coordinates
-getVistedSet (State _ _ s _) = s
+getVistedSet :: State -> Set.Set Vector
+getVistedSet (State _ _ s) = s
