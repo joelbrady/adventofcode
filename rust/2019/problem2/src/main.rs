2a2d348eb2022c0b790b575c39b23bdd867c874e
Joel Brady <jbrady@atlassian.com>
2019-12-09 23:00:44 +1100

    move intcode to library rust

--- rust/2019/problem2/src/main.rs
+++ rust/2019/problem2/src/main.rs
@@ -1,7 +1,8 @@
-use std::io::{stdin, Read};
+use input::get_input;
+use intcode::{parse_program, Machine};
 
 fn main() {
-    let input = get_input();
+    let input = get_input("input");
     let solution = solve(&input, 12, 2);
 
     println!("The solution to part 1 is {}", solution);
@@ -10,21 +11,22 @@ fn main() {
     println!("The solution to part 2 is {}", part_2);
 }
 
-fn solve(s: &str, noun: usize, verb: usize) -> usize {
-    let parsed_input = parse(s);
-    let mut m = Machine::new(&parsed_input, noun, verb);
+fn solve(s: &str, noun: i32, verb: i32) -> i32 {
+    let parsed_input = parse_program(s);
+    let mut m = Machine::new_with_noun_verb(&parsed_input, noun, verb);
     m.run_until_halt();
     m.get_value_at_addr(0)
 }
 
-const EXPECTED: usize = 19690720;
+const EXPECTED: i32 = 19690720;
 
-fn solve_b(s: &str) -> usize {
-    let parsed_input = parse(s);
+fn solve_b(s: &str) -> i32 {
+    let parsed_input = parse_program(s);
     let mut answer = 0;
     for noun in 0..100 {
         for verb in 0..100 {
-            let mut m = Machine::new(&parsed_input, noun, verb);
+            let mut m = Machine::new_with_noun_verb(&parsed_input, noun, verb);
+
             m.run_until_halt();
             let a = m.get_value_at_addr(0);
             if a == EXPECTED {
@@ -36,56 +38,19 @@ fn solve_b(s: &str) -> usize {
     answer
 }
 
-fn parse(s: &str) -> Vec<usize> {
-    s.split(',')
-        .into_iter()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect()
-}
+#[cfg(test)]
+mod test {
+    use super::*;
 
-fn get_input() -> String {
-    let mut string = String::new();
-    stdin().lock()
-        .read_to_string(&mut string)
-        .unwrap();
-    string
-}
+    #[test]
+    fn test_solutions() {
+        let input = get_input("input");
+        let solution = solve(&input, 12, 2);
 
-struct Machine {
-    ip: usize,
-    memory: [usize; 10000]
-}
+        assert_eq!(solution, 4090701);
 
-impl Machine {
-    fn new(initial_memory: &[usize], noun: usize, verb: usize) -> Machine {
-        let mut memory = [0; 10000];
-        for i in 0..initial_memory.len() {
-            memory[i] = initial_memory[i] as usize;
-        }
-        memory[1] = noun;
-        memory[2] = verb;
-        Machine { ip: 0, memory }
-    }
+        let part_2 = solve_b(&input);
 
-    fn run_until_halt(&mut self) {
-        loop {
-            let ip = self.ip;
-            let instruction = self.memory[ip];
-            let a = self.memory[ip + 1];
-            let b = self.memory[ip + 2];
-            let c = self.memory[ip + 3] as usize;
-            match instruction {
-                1 => self.memory[c] = self.memory[a] + self.memory[b],
-                2 => self.memory[c] = self.memory[a] * self.memory[b],
-                99 => break,
-                _ => unimplemented!(),
-            }
-            self.ip += 4
-        }
-    }
-
-    fn get_value_at_addr(&self, addr: usize) -> usize {
-        self.memory[addr]
+        assert_eq!(part_2, 6421);
     }
 }
-
---
+++ rust/intcode/src/lib.rs
@@ -0,0 +1,327 @@
+pub struct Machine {
+    ip: usize,
+    memory: [i32; 10000],
+    input: Vec<i32>,
+    output: i32,
+}
+
+impl Machine {
+    pub fn new(initial_memory: &[i32], input: &[i32]) -> Machine {
+        let mut memory = [0; 10000];
+        for i in 0..initial_memory.len() {
+            memory[i] = initial_memory[i];
+        }
+        Machine { ip: 0, memory, input: Vec::from(input), output: 0 }
+    }
+
+    pub fn new_with_noun_verb(initial_memory: &[i32], noun: i32, verb: i32) -> Machine {
+        let mut m = Machine::new(initial_memory, &vec![0]);
+        m.memory[1] = noun;
+        m.memory[2] = verb;
+        m
+    }
+
+    pub fn run_until_halt(&mut self) -> i32 {
+        loop {
+            let op = self.fetch(self.ip);
+//            println!("running {:?}", op);
+            let (halted, ip_update) = self.execute(&op);
+            self.update_ip(&ip_update);
+            match halted {
+                true => break,
+                _ => continue,
+            }
+        }
+        self.output
+    }
+
+    pub fn get_value_at_addr(&self, addr: usize) -> i32 {
+        self.memory[addr]
+    }
+
+    fn update_ip(&mut self, update: &IpUpdate) {
+        match update {
+            IpUpdate::Relative(n) => self.ip += *n as usize,
+            IpUpdate::Absolute(n) => self.ip = *n,
+        }
+    }
+
+    fn fetch(&self, addr: usize) -> Opcode {
+        decode_opcode(&self.memory[addr..addr + 4])
+    }
+
+    fn execute(&mut self, op: &Opcode) -> (bool, IpUpdate) {
+        use Opcode::*;
+        use IpUpdate::*;
+
+        if self.output != 0 {
+            if let Halt = op {
+//                println!("test passed")
+            } else {
+                panic!("test failed")
+            }
+        }
+        match op {
+            Add(a, b, c) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+//                println!("{} + {} = {}", a, b, a + b);
+                self.write(c, a + b);
+                (false, Relative(4))
+            }
+            Multiply(a, b, c) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+//                println!("{} * {} = {}", a, b, a * b);
+                self.write(c, a * b);
+                (false, Relative(4))
+            }
+            Input(addr) => {
+                let a = self.input.remove(0);
+                self.write(addr, a);
+                (false, Relative(2))
+            }
+            Output(addr) => {
+                let value = self.evaluate_param(addr);
+//                println!("output {}", value);
+                self.output = value;
+                (false, Relative(2))
+            }
+            JumpIfTrue(a, b) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+                let branch = if a != 0 {
+                    Absolute(b as usize)
+                } else {
+                    Relative(3)
+                };
+                (false, branch)
+            }
+            JumpIfFalse(a, b) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+                let branch = if a == 0 {
+                    Absolute(b as usize)
+                } else {
+                    Relative(3)
+                };
+                (false, branch)
+            }
+            Halt => (true, Relative(1)),
+            LessThan(a, b, c) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+                let result = if a < b {
+                    1
+                } else {
+                    0
+                };
+                self.write(c, result);
+                (false, Relative(4))
+            }
+            EqualTo(a, b, c) => {
+                let a = self.evaluate_param(a);
+                let b = self.evaluate_param(b);
+                let result = if a == b {
+                    1
+                } else {
+                    0
+                };
+                self.write(c, result);
+                (false, Relative(4))
+            }
+        }
+    }
+
+    fn write(&mut self, addr: &Parameter, value: i32) {
+        use Parameter::*;
+
+        match addr {
+            Position(addr) => {
+//                println!("writing {} to *{}", value, *addr);
+                self.memory[*addr] = value
+            }
+            _ => panic!("Writing to immediate parameter not supported"),
+        }
+    }
+
+    fn evaluate_param(&self, p: &Parameter) -> i32 {
+        use Parameter::*;
+
+        match p {
+            Immediate(value) => *value,
+            Position(addr) => self.memory[*addr],
+        }
+    }
+}
+
+pub fn parse_program(s: &str) -> Vec<i32> {
+    s.split(',')
+        .into_iter()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Opcode {
+    Add(Parameter, Parameter, Parameter),
+    Multiply(Parameter, Parameter, Parameter),
+    Input(Parameter),
+    Output(Parameter),
+    Halt,
+    JumpIfTrue(Parameter, Parameter),
+    JumpIfFalse(Parameter, Parameter),
+    LessThan(Parameter, Parameter, Parameter),
+    EqualTo(Parameter, Parameter, Parameter),
+}
+
+enum IpUpdate {
+    Relative(i32),
+    Absolute(usize),
+}
+
+fn decode_opcode(code: &[i32]) -> Opcode {
+    let a = &code[0];
+    let t = a % 100;
+    let params = &code[1..];
+    match t {
+        1 => decode_add(a, params),
+        2 => decode_multiply(a, params),
+        3 => decode_input(a, params),
+        4 => decode_output(a, params),
+        5 => decode_jump_if_true(a, params),
+        6 => decode_jump_if_false(a, params),
+        7 => decode_less_than(a, params),
+        8 => decode_equal_to(a, params),
+        99 => Opcode::Halt,
+        op => panic!("Unknown opcode: {}", op)
+    }
+}
+
+fn decode_add(a: &i32, ps: &[i32]) -> Opcode {
+    let (p0, p1, p2) = get_arithmetic_params(a, ps);
+
+    Opcode::Add(p0, p1, p2)
+}
+
+fn decode_multiply(a: &i32, ps: &[i32]) -> Opcode {
+    let (p0, p1, p2) = get_arithmetic_params(a, ps);
+
+    Opcode::Multiply(p0, p1, p2)
+}
+
+fn decode_input(a: &i32, ps: &[i32]) -> Opcode {
+    Opcode::Input(get_input_param(a, ps))
+}
+
+fn decode_output(a: &i32, ps: &[i32]) -> Opcode {
+    Opcode::Output(get_output_param(a, ps))
+}
+
+fn decode_jump_if_true(a: &i32, ps: &[i32]) -> Opcode {
+    let (p0, p1) = get_jump_params(a, ps);
+
+    Opcode::JumpIfTrue(p0, p1)
+}
+
+fn decode_jump_if_false(a: &i32, ps: &[i32]) -> Opcode {
+    // if the first parameter is non-zero, it sets the instruction pointer to
+    // the value from the second parameter. Otherwise, it does nothing.
+    let (p0, p1) = get_jump_params(a, ps);
+
+    Opcode::JumpIfFalse(p0, p1)
+}
+
+fn decode_less_than(a: &i32, ps: &[i32]) -> Opcode {
+    // if the first parameter is non-zero, it sets the instruction pointer to
+    // the value from the second parameter. Otherwise, it does nothing.
+    let (p0, p1, p2) = get_arithmetic_params(a, ps);
+
+    Opcode::LessThan(p0, p1, p2)
+}
+
+fn decode_equal_to(a: &i32, ps: &[i32]) -> Opcode {
+    // if the first parameter is non-zero, it sets the instruction pointer to
+    // the value from the second parameter. Otherwise, it does nothing.
+    let (p0, p1, p2) = get_arithmetic_params(a, ps);
+
+    Opcode::EqualTo(p0, p1, p2)
+}
+
+fn get_arithmetic_params(a: &i32, ps: &[i32]) -> (Parameter, Parameter, Parameter) {
+    let p0_type = (a / 100) % 10;
+    let p1_type = (a / 1000) % 10;
+    let p2_type = (a / 10000) % 10;
+
+    let p0 = decode_param(p0_type, ps[0]);
+    let p1 = decode_param(p1_type, ps[1]);
+    let p2 = decode_param(p2_type, ps[2]);
+
+    (p0, p1, p2)
+}
+
+fn get_input_param(a: &i32, ps: &[i32]) -> Parameter {
+    let mode = *a / 100;
+    if mode != 0 {
+        eprintln!("mode was {}", *a);
+        panic!("input param mode must be position")
+    }
+    decode_param(0, ps[0])
+}
+
+fn get_output_param(a: &i32, ps: &[i32]) -> Parameter {
+    let mode = *a / 100;
+    decode_param(mode, ps[0])
+}
+
+fn get_jump_params(a: &i32, ps: &[i32]) -> (Parameter, Parameter) {
+    let p0_type = (a / 100) % 10;
+    let p1_type = (a / 1000) % 10;
+
+    let p0 = decode_param(p0_type, ps[0]);
+    let p1 = decode_param(p1_type, ps[1]);
+
+    (p0, p1)
+}
+
+fn decode_param(t: i32, v: i32) -> Parameter {
+    use Parameter::*;
+
+    match t {
+        0 => Position(v as usize),
+        1 => Immediate(v),
+        _ => unimplemented!(),
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Parameter {
+    Immediate(i32),
+    Position(usize),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_decode_opcode() {
+        use Opcode::*;
+        use Parameter::*;
+
+        assert_eq!(decode_opcode(&[1, 0, 0, 0]), Add(Position(0), Position(0), Position(0)));
+        assert_eq!(decode_opcode(&[1101, 0, 0, 0]), Add(Immediate(0), Immediate(0), Position(0)));
+        assert_eq!(decode_opcode(&[102, 0, 13, 0]), Multiply(Immediate(0), Position(13), Position(0)));
+        assert_eq!(decode_opcode(&[03, 1231]), Input(Position(1231)));
+        assert_eq!(decode_opcode(&[04, 1111]), Output(Position(1111)));
+        assert_eq!(decode_opcode(&[104, 1111]), Output(Immediate(1111)));
+    }
+
+    #[test]
+    fn test_io() {
+        let program = [3, 0, 4, 0, 99];
+        let mut m = Machine::new(&program, &vec![42]);
+        let result = m.run_until_halt();
+        assert_eq!(result, 42)
+    }
+}
