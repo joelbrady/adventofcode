56cfabd6db7e7122f0f4bb4620bee93929c25cf2
Joel Brady <joel.brady1@gmail.com>
2020-12-05 13:24:45 +1100

    clean up

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,9 +5,9 @@ mod graph;
 mod parse;
 
 fn main() {
-    // call_2018();
+    call_2018();
     call_2019();
-    // call_2020();
+    call_2020();
 }
 
 fn call_2018() {
@@ -17,23 +17,23 @@ fn call_2018() {
 }
 
 fn call_2019() {
-    // cal2019::day01::main();
-    // cal2019::day02::main();
-    // cal2019::day03::main();
-    // cal2019::day04::main();
-    // cal2019::day05::main();
-    // cal2019::day06::main();
-    // cal2019::day07::main();
-    // cal2019::day08::main();
-    // cal2019::day09::main();
-    // cal2019::day10::main();
-    // cal2019::day11::main();
-    // cal2019::day12::main();
-    // cal2019::day13::main();
-    // cal2019::day14::main();
-    // cal2019::day15::main();
-    // cal2019::day16::main();
-    // cal2019::day17::main();
+    cal2019::day01::main();
+    cal2019::day02::main();
+    cal2019::day03::main();
+    cal2019::day04::main();
+    cal2019::day05::main();
+    cal2019::day06::main();
+    cal2019::day07::main();
+    cal2019::day08::main();
+    cal2019::day09::main();
+    cal2019::day10::main();
+    cal2019::day11::main();
+    cal2019::day12::main();
+    cal2019::day13::main();
+    cal2019::day14::main();
+    cal2019::day15::main();
+    cal2019::day16::main();
+    cal2019::day17::main();
     cal2019::day18::main();
 }
 
