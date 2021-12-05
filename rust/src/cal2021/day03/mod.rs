1a4f5b4d034f7f1e5b1fcaa9de341f5a9f6cf471
Joel Brady <joel.brady1@gmail.com>
2021-12-05 17:47:26 +1100

    2021 day 3 part 2

--- rust/src/cal2021/day03/mod.rs
+++ rust/src/cal2021/day03/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
 use itertools::Itertools;
@@ -21,7 +21,7 @@ struct Input {
     ns: Vec<BinaryNumber>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
 struct BinaryNumber(Vec<BinaryDigit>);
 
 impl BinaryNumber {
@@ -44,6 +44,14 @@ impl From<BinaryNumber> for String {
                 BinaryDigit::One => '1',
             })
             .collect()
+    }
+}
+
+impl From<BinaryNumber> for i64 {
+    fn from(n: BinaryNumber) -> Self {
+        let s: String = n.into();
+
+        i64::from_str_radix(&s, 2).unwrap()
     }
 }
 
@@ -76,16 +84,14 @@ fn parse_binary_number(s: &str) -> BinaryNumber {
 }
 
 fn solve(input: &Input) -> i64 {
-    let length = input.ns[0].0.len();
+    let ns = &input.ns;
+    let length = ns[0].0.len();
 
-    let digits_by_position = (0..length)
-        .flat_map(|i| input.ns.iter()
-            .map(move |n| (i, n.0[i])))
-        .into_group_map();
+    let digits_by_position: HashMap<usize, Vec<BinaryDigit>> = get_digits_by_position(ns);
 
     let most_common_digit_by_position: HashMap<usize, BinaryDigit> = digits_by_position
         .iter()
-        .map(|(k, v)| (*k, most_common(v)))
+        .map(|(k, v)| (*k, *most_common(v)[0]))
         .collect();
 
     let binary_number: Vec<BinaryDigit> = (0..length)
@@ -94,29 +100,94 @@ fn solve(input: &Input) -> i64 {
         .collect();
 
     let binary_number = BinaryNumber(binary_number);
-
-    let gamma_rate: String = binary_number.clone().into();
-
-    let gamma_rate = i64::from_str_radix(&gamma_rate, 2).unwrap();
-
     let epsilon_rate = binary_number.invert();
-    let epsilon_rate: String = epsilon_rate.into();
-    let epsilon_rate = i64::from_str_radix(&epsilon_rate, 2).unwrap();
+
+    let gamma_rate: i64 = binary_number.into();
+    let epsilon_rate: i64 = epsilon_rate.into();
 
     gamma_rate * epsilon_rate
 }
 
-fn most_common<T: Eq + Hash + Copy>(ts: &[T]) -> T {
-    ts.iter()
-        .counts()
-        .iter()
-        .max_by_key(|(_, count)| *count)
-        .map(|(t, _)| **t)
-        .unwrap()
+fn get_digits_by_position(ns: &[BinaryNumber]) -> HashMap<usize, Vec<BinaryDigit>> {
+    let length = ns[0].0.len();
+
+    (0..length)
+        .flat_map(|i| ns.iter()
+            .map(move |n| (i, n.0[i])))
+        .into_group_map()
 }
 
-fn solve2(_: &Input) -> i64 {
-    todo!()
+fn most_common<T: Eq + Hash>(ts: &[T]) -> Vec<&T> {
+    let counts = ts.iter()
+        .counts();
+
+    let max_count = counts.values().max().unwrap();
+
+    counts.iter()
+        .filter(|(_, count)| **count == *max_count)
+        .map(|(t, _)| *t)
+        .collect()
+}
+
+fn least_common<T: Eq + Hash>(ts: &[T]) -> Vec<&T> {
+    let counts = ts.iter()
+        .counts();
+
+    let min_count = counts.values().min().unwrap();
+
+    counts.iter()
+        .filter(|(_, count)| **count == *min_count)
+        .map(|(t, _)| *t)
+        .collect()
+}
+
+fn solve2(input: &Input) -> i64 {
+    let oxygen = filter_oxygen(&input.ns);
+    let oxygen: i64 = oxygen.into();
+
+    let co2 = filter_co2(&input.ns);
+    let co2: i64 = co2.into();
+
+    oxygen * co2
+}
+
+fn filter_oxygen(ns: &[BinaryNumber]) -> BinaryNumber {
+    filter_generic(ns, most_common, &BinaryDigit::One)
+}
+
+fn filter_co2(ns: &[BinaryNumber]) -> BinaryNumber {
+    filter_generic(ns, least_common, &BinaryDigit::Zero)
+}
+
+fn filter_generic<F>(ns: &[BinaryNumber], f: F, default_on_tie: &BinaryDigit) -> BinaryNumber
+    where F: Fn(&[BinaryDigit]) -> Vec<&BinaryDigit> {
+    let mut candidates: HashSet<BinaryNumber> = ns.iter().cloned().collect();
+
+    assert!(!ns.is_empty());
+    while candidates.len() > 1 {
+        for i in 0..(ns[0].0.len()) {
+            let current: Vec<BinaryNumber> = candidates.iter().cloned().collect();
+            let by_position = get_digits_by_position(&current);
+            let filter_value = by_position.get(&i).unwrap();
+            let filter_value = f(filter_value);
+            let filter_value = if filter_value.len() == 1 {
+                *filter_value[0]
+            } else {
+                *default_on_tie
+            };
+            candidates = candidates.iter()
+                .filter(|n| if n.0[i] == filter_value {
+                    println!("keeping {:?}", n);
+                    true
+                } else {
+                    false
+                })
+                .cloned()
+                .collect();
+        }
+    }
+
+    candidates.into_iter().next().unwrap()
 }
 
 #[cfg(test)]
@@ -141,6 +212,28 @@ mod test {
 
         let expected = 4191876;
         let actual = solve(&input);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_part2_example1() {
+        let input = include_str!("example1");
+        let input = parse_input(input);
+        let expected = 230;
+
+        let actual = solve2(&input);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_solution2() {
+        let input = include_str!("input");
+        let input = parse_input(input);
+
+        let expected = 3414905;
+        let actual = solve2(&input);
 
         assert_eq!(actual, expected)
     }
