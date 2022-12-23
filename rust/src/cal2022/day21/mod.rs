b924e13ca367b1e3e6c9df051cb87510fb543ae9
Joel Brady <joel.brady1@gmail.com>
2022-12-24 08:02:48 +1100

    2022 day 21 part 2

--- rust/src/cal2022/day21/mod.rs
+++ rust/src/cal2022/day21/mod.rs
@@ -33,11 +33,12 @@ struct Monkey {
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 enum Expr {
-    Number(i64),
+    Number(i128),
     Add(String, String),
     Sub(String, String),
     Mul(String, String),
     Div(String, String),
+    Human,
 }
 
 fn parse_input(s: &str) -> Input {
@@ -67,7 +68,7 @@ fn parse_expression(s: &str) -> IResult<&str, Expr> {
 }
 
 fn parse_number(s: &str) -> IResult<&str, Expr> {
-    let (rem, n) = nom::character::complete::i64(s)?;
+    let (rem, n) = nom::character::complete::i128(s)?;
 
     Ok((rem, Expr::Number(n)))
 }
@@ -89,6 +90,7 @@ fn parse_mul(s: &str) -> IResult<&str, Expr> {
 
     Ok((rem, Expr::Mul(a.to_owned(), b.to_owned())))
 }
+
 fn parse_div(s: &str) -> IResult<&str, Expr> {
     let (rem, (a, b)) = separated_pair(alpha1, tag(" / "), alpha1)(s)?;
 
@@ -96,7 +98,7 @@ fn parse_div(s: &str) -> IResult<&str, Expr> {
 }
 
 
-fn solve_part1(input: &Input) -> i64 {
+fn solve_part1(input: &Input) -> i128 {
     let map: HashMap<String, Expr> = input.monkeys.iter()
         .cloned()
         .map(|m| (m.name, m.expression))
@@ -104,41 +106,201 @@ fn solve_part1(input: &Input) -> i64 {
 
     let root_expr = map.get("root").unwrap();
 
-    eval(root_expr, &map)
+    if let SExpr::Number(n) = simplify(root_expr, &map) {
+        n
+    } else {
+        panic!()
+    }
 }
 
-fn eval(expr: &Expr, map: &HashMap<String, Expr>) -> i64 {
-    match expr {
-        Expr::Number(n) => *n,
-        Expr::Add(a, b) => {
-            let a = map.get(a).unwrap();
-            let b = map.get(b).unwrap();
+#[derive(Debug, Clone)]
+enum SExpr {
+    Number(i128),
+    Add(Box<SExpr>, Box<SExpr>),
+    Sub(Box<SExpr>, Box<SExpr>),
+    Mul(Box<SExpr>, Box<SExpr>),
+    Div(Box<SExpr>, Box<SExpr>),
+    Human,
+}
 
-            eval(a, map) + eval(b, map)
-        }
-        Expr::Sub(a, b) => {
-            let a = map.get(a).unwrap();
-            let b = map.get(b).unwrap();
-
-            eval(a, map) - eval(b, map)
-        }
-        Expr::Mul(a, b) => {
-            let a = map.get(a).unwrap();
-            let b = map.get(b).unwrap();
-
-            eval(a, map) * eval(b, map)
-        }
-        Expr::Div(a, b) => {
-            let a = map.get(a).unwrap();
-            let b = map.get(b).unwrap();
-
-            eval(a, map) / eval(b, map)
+impl SExpr {
+    fn assume_num(&self) -> i128 {
+        match self {
+            SExpr::Number(n) => *n,
+            _ => panic!(),
         }
     }
 }
 
-fn solve_part2(_input: &Input) -> i64 {
-    todo!()
+fn simplify(expr: &Expr, map: &HashMap<String, Expr>) -> SExpr {
+    match expr {
+        Expr::Number(n) => SExpr::Number(*n),
+        Expr::Add(a, b) => {
+            let a = map.get(a).unwrap();
+            let a = simplify(a, map);
+            let b = map.get(b).unwrap();
+            let b = simplify(b, map);
+
+            match a {
+                SExpr::Number(a) => {
+                    match b {
+                        SExpr::Number(b) => SExpr::Number(a + b),
+                        e => SExpr::Add(Box::new(SExpr::Number(a)), Box::new(e)),
+                    }
+                }
+                e => SExpr::Add(Box::new(e), Box::new(b))
+            }
+        }
+        Expr::Sub(a, b) => {
+            let a = map.get(a).unwrap();
+            let a = simplify(a, map);
+            let b = map.get(b).unwrap();
+            let b = simplify(b, map);
+
+            match a {
+                SExpr::Number(a) => {
+                    match b {
+                        SExpr::Number(b) => SExpr::Number(a - b),
+                        e => SExpr::Sub(Box::new(SExpr::Number(a)), Box::new(e)),
+                    }
+                }
+                e => SExpr::Sub(Box::new(e), Box::new(b))
+            }
+        }
+        Expr::Mul(a, b) => {
+            let a = map.get(a).unwrap();
+            let a = simplify(a, map);
+            let b = map.get(b).unwrap();
+            let b = simplify(b, map);
+
+            match a {
+                SExpr::Number(a) => {
+                    match b {
+                        SExpr::Number(b) => SExpr::Number(a * b),
+                        e => SExpr::Mul(Box::new(SExpr::Number(a)), Box::new(e)),
+                    }
+                }
+                e => SExpr::Mul(Box::new(e), Box::new(b))
+            }
+        }
+        Expr::Div(a, b) => {
+            let a = map.get(a).unwrap();
+            let a = simplify(a, map);
+            let b = map.get(b).unwrap();
+            let b = simplify(b, map);
+
+            match a {
+                SExpr::Number(a) => {
+                    match b {
+                        SExpr::Number(b) => SExpr::Number(a / b),
+                        e => SExpr::Div(Box::new(SExpr::Number(a)), Box::new(e)),
+                    }
+                }
+                e => SExpr::Div(Box::new(e), Box::new(b))
+            }
+        }
+        Expr::Human => SExpr::Human,
+    }
+}
+
+fn solve_part2(input: &Input) -> i128 {
+    let map = {
+        let mut map: HashMap<String, Expr> = input.monkeys.iter()
+            .cloned()
+            .map(|m| (m.name, m.expression))
+            .collect();
+
+        map.insert("humn".to_owned(), Expr::Human);
+        map
+    };
+
+    let map: HashMap<String, SExpr> = {
+        let old = map.clone();
+        map.into_iter()
+            .map(|(k, v)| (k, simplify(&v, &old)))
+            .collect()
+    };
+
+    let root = map.get("root").unwrap();
+    let (left, right) = match root {
+        SExpr::Add(a, b) => (a, b),
+        SExpr::Sub(a, b) => (a, b),
+        SExpr::Mul(a, b) => (a, b),
+        SExpr::Div(a, b) => (a, b),
+        _ => panic!("Root did not contain two terms"),
+    };
+
+    let (e, target) = if contains_human(left) {
+        match right.as_ref() {
+            SExpr::Number(n) => (left, n),
+            _ => panic!("other side contained human input"),
+        }
+    } else {
+        match left.as_ref() {
+            SExpr::Number(n) => (right, n),
+            _ => panic!("other side contained human input"),
+        }
+    };
+
+    let mut expr: SExpr = e.as_ref().clone();
+    let mut target = *target;
+
+    loop {
+        let next_expr = match &expr {
+            SExpr::Number(n) => return *n,
+            SExpr::Add(a, b) => {
+                if contains_human(a.as_ref()) {
+                    target -= b.assume_num();
+                    a.as_ref().clone()
+                } else {
+                    target -= a.assume_num();
+                    b.as_ref().clone()
+                }
+            }
+            SExpr::Sub(a, b) => {
+                if contains_human(a.as_ref()) {
+                    target += b.assume_num();
+                    a.as_ref().clone()
+                } else {
+                    target -= a.assume_num();
+                    target *= -1;
+                    b.as_ref().clone()
+                }
+            }
+            SExpr::Mul(a, b) => {
+                if contains_human(a.as_ref()) {
+                    target /= b.assume_num();
+                    a.as_ref().clone()
+                } else {
+                    target /= a.assume_num();
+                    b.as_ref().clone()
+                }
+            }
+            SExpr::Div(a, b) => {
+                if contains_human(a.as_ref()) {
+                    target *= b.assume_num();
+                    a.as_ref().clone()
+                } else {
+                    target *= a.assume_num();
+                    b.as_ref().clone()
+                }
+            }
+            SExpr::Human => return target,
+        };
+
+        expr = next_expr;
+    }
+}
+
+fn contains_human(e: &SExpr) -> bool {
+    match e {
+        SExpr::Number(_) => false,
+        SExpr::Add(a, b) => contains_human(a) || contains_human(b),
+        SExpr::Sub(a, b) => contains_human(a) || contains_human(b),
+        SExpr::Mul(a, b) => contains_human(a) || contains_human(b),
+        SExpr::Div(a, b) => contains_human(a) || contains_human(b),
+        SExpr::Human => true,
+    }
 }
 
 #[cfg(test)]
@@ -182,7 +344,7 @@ mod test {
     #[test]
     fn test_solve_part2_example() {
         let input = parse_input(include_str!("example"));
-        let expected = 0;
+        let expected = 301;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
@@ -191,7 +353,7 @@ mod test {
     #[test]
     fn test_solve_part2() {
         let input = parse_input(include_str!("input"));
-        let expected = 0;
+        let expected = 3429411069028;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
