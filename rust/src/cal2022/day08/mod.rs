954ccdc8023930ca87afc79885e79f622e86c83b
Joel Brady <joel.brady1@gmail.com>
2022-12-27 21:53:47 +1100

    2022 day 08 part 2

--- rust/src/cal2022/day08/mod.rs
+++ rust/src/cal2022/day08/mod.rs
@@ -77,8 +77,54 @@ fn in_bounds((y, x): (i32, i32), (max_row, max_col): (i32, i32)) -> bool {
     y >= 0 && y <= max_row && x >= 0 && x <= max_col
 }
 
-fn solve_part2(_input: &Input) -> i64 {
-    todo!()
+fn solve_part2(input: &Input) -> u32 {
+    let n_rows = input.trees.len() as i32;
+    let n_cols = input.trees[0].len() as i32;
+
+    let mut scores = vec![];
+
+    for row in 0..n_rows {
+        for col in 0..n_cols {
+            scores.push(score(row, col, input));
+        }
+    }
+
+    scores.into_iter().max().unwrap()
+}
+
+fn score(row: i32, col: i32, input: &Input) -> u32 {
+    let trees = &input.trees;
+
+    let max_row = (trees.len() - 1) as i32;
+    let max_col = (trees[0].len() - 1) as i32;
+
+    let bounds = (max_row, max_col);
+
+    let tree = trees[row as usize][col as usize];
+
+    let scores: [u32; 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        .map(|(dy, dx)| score_direction(row, col, trees, bounds, tree, dy, dx));
+
+    scores.into_iter().product()
+}
+
+fn score_direction(row: i32, col: i32, trees: &[Vec<u32>], bounds: (i32, i32), tree: u32, dy: i32, dx: i32) -> u32 {
+    let mut pos = (row + dy, col + dx);
+    let mut score = 1;
+    while in_bounds(pos, bounds) {
+        let other = trees[pos.0 as usize][pos.1 as usize];
+        if other >= tree {
+            break;
+        }
+
+        pos = (pos.0 + dy, pos.1 + dx);
+        score += 1;
+    }
+
+    if !in_bounds(pos, bounds) {
+        score -= 1;
+    }
+    score
 }
 
 #[cfg(test)]
@@ -104,9 +150,45 @@ mod test {
     }
 
     #[test]
+    fn test_up_part2_example() {
+        let input = parse_input(include_str!("example"));
+        let expected = 1;
+        let actual = score_direction(1, 2, &input.trees, (4, 4), 5, -1, 0);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_left_part2_example() {
+        let input = parse_input(include_str!("example"));
+        let expected = 1;
+        let actual = score_direction(1, 2, &input.trees, (4, 4), 5, 0, -1);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_right_part2_example() {
+        let input = parse_input(include_str!("example"));
+        let expected = 2;
+        let actual = score_direction(1, 2, &input.trees, (4, 4), 5, 0, 1);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_down_part2_example() {
+        let input = parse_input(include_str!("example"));
+        let expected = 2;
+        let actual = score_direction(1, 2, &input.trees, (4, 4), 5, 1, 0);
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
     fn test_solve_part2_example() {
         let input = parse_input(include_str!("example"));
-        let expected = 0;
+        let expected = 8;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
@@ -115,7 +197,7 @@ mod test {
     #[test]
     fn test_solve_part2() {
         let input = parse_input(include_str!("input"));
-        let expected = 0;
+        let expected = 536625;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
