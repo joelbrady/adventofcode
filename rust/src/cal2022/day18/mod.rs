593a0082405a99aec7793be103644315813cbb52
Joel Brady <joel.brady1@gmail.com>
2023-01-21 13:50:59 +1100

    2022 day 18 part 2

--- rust/src/cal2022/day18/mod.rs
+++ rust/src/cal2022/day18/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use nom::bytes::complete::tag;
 use nom::character::complete::line_ending;
@@ -39,11 +39,10 @@ fn parse_cube(s: &str) -> IResult<&str, (i64, i64, i64)> {
 }
 
 fn solve_part1(input: &Input) -> i64 {
-    let dirs = dirs();
     let cubes: HashSet<_> = input.cubes.iter().copied().collect();
     cubes.iter()
         .map(|(x, y, z)| {
-            let sides_covered = dirs.iter()
+            let sides_covered = dirs().iter()
                 .map(|(dx, dy, dz)| (x + dx, y + dy, z + dz))
                 .map(|(x, y, z)| i64::from(cubes.contains(&(x, y, z))))
                 .sum::<i64>();
@@ -52,8 +51,8 @@ fn solve_part1(input: &Input) -> i64 {
         .sum()
 }
 
-fn dirs() -> Vec<(i64, i64, i64)> {
-    vec![
+fn dirs() -> &'static [(i64, i64, i64)] {
+    &[
         (-1, 0, 0),
         (1, 0, 0),
         (0, -1, 0),
@@ -63,8 +62,75 @@ fn dirs() -> Vec<(i64, i64, i64)> {
     ]
 }
 
-fn solve_part2(_input: &Input) -> i64 {
-    todo!()
+fn solve_part2(input: &Input) -> i64 {
+    let outside: HashSet<(i64, i64, i64)> = flood_outside(&input.cubes);
+
+    input.cubes.iter()
+        .map(|(x, y, z)| {
+            let sides_touching_outside = dirs().iter()
+                .map(|(dx, dy, dz)| (x + dx, y + dy, z + dz))
+                .filter(|(x, y, z)| outside.contains(&(*x, *y, *z)))
+                .count() as i64;
+            sides_touching_outside
+        })
+        .sum()
+}
+
+fn flood_outside(cubes: &[(i64, i64, i64)]) -> HashSet<(i64, i64, i64)> {
+    let (min , max) = bounding_box(cubes);
+    let cubes: HashSet<(i64, i64, i64)> = cubes.iter().copied().collect();
+    let (minx, miny, minz) = add(min, (-1, -1, -1));
+    let (maxx, maxy, maxz) = add(max, (1, 1, 1));
+
+    let mut outside = HashSet::new();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((minx, miny, minz));
+
+    while let Some(current) = queue.pop_front() {
+        if outside.contains(&current) || cubes.contains(&current) {
+            continue;
+        }
+
+        let (x, y, z) = current;
+        if x < minx || y < miny || z < minz || x > maxx || y > maxy || z > maxz {
+            continue;
+        }
+
+        let children = dirs().iter()
+            .map(|(dx, dy, dz)| (x + dx, y + dy, z + dz));
+
+        queue.extend(children);
+        outside.insert(current);
+    }
+
+    outside
+}
+
+fn add((ax, ay, az): (i64, i64, i64), (bx, by, bz): (i64, i64, i64)) -> (i64, i64, i64) {
+    (ax + bx, ay + by, az + bz)
+}
+
+fn bounding_box(cubes: &[(i64, i64, i64)]) -> ((i64, i64, i64), (i64, i64, i64)) {
+    let mut minx = i64::MAX;
+    let mut miny = i64::MAX;
+    let mut minz = i64::MAX;
+    
+    let mut maxx = i64::MIN;
+    let mut maxy = i64::MIN;
+    let mut maxz = i64::MIN;
+    
+    for (x, y, z) in cubes {
+        minx = minx.min(*x);
+        miny = miny.min(*y);
+        minz = minz.min(*z);
+
+        maxx = maxx.max(*x);
+        maxy = maxy.max(*y);
+        maxz = maxz.max(*z);
+    }
+
+    ((minx, miny, minz), (maxx, maxy, maxz))
 }
 
 #[cfg(test)]
@@ -92,7 +158,7 @@ mod test {
     #[test]
     fn test_solve_part2_example() {
         let input = parse_input(include_str!("example"));
-        let expected = 0;
+        let expected = 58;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
@@ -101,7 +167,7 @@ mod test {
     #[test]
     fn test_solve_part2() {
         let input = parse_input(include_str!("input"));
-        let expected = 0;
+        let expected = 2428;
         let actual = solve_part2(&input);
 
         assert_eq!(actual, expected)
